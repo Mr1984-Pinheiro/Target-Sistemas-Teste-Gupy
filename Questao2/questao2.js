55559828d9adfabdd2499bf26b2259ecf1c04d15
Carlos Eduardo Tavares Pinheiro <cadutapinheiro@gmail.com>
2022-10-31 21:49:54 -0300

    remove comentarios

--- Questao2/questao2.js
+++ Questao2/questao2.js
@@ -9,65 +9,7 @@ IMPORTANTE:
 Esse número pode ser informado através de qualquer entrada
  de sua preferência ou pode ser previamente definido no código;*/
 
-
- // program to generate fibonacci series up to n terms
-
- /*const fibonacci = function (result, len) {
-  let num1 = result[0]
-  let num2 = result[1]
-  let next = 0
-  let counts = 2
-
-  while (counts < len) {
-    next = num1 + num2
-    num1 = num2
-    num2 = next
-    result.push(next)
-    counts++
-  }
-
-  return result;
-
-
- }*/
-
- //Fibonacci Series using Recursion
- /*let n = 9;
-     
- // function returns the Fibonacci number
- function fib(n) {
- if (n <= 1)
-     return n;
- return fib(n-1) + fib(n-2);
-}
-
- //function call
- document.write(fib(n));
-//This code is contributed by Surbhi Tyagi*/
-
-/*
-let termo1 = 0
-let termo2 = 1
-let termo3 = 0
-
-let valor = 5 
-
-while (valor > termo3 ) {
-    termo3 = termo1 + termo2
-    termo1 = termo2
-    termo2 = termo3
-}
-
-if (valor == 0 || valor == 1) {
-   alert('O número faz parte da sequência de Fibonacci.');
-} if (valor == termo3) {
-    alert('O número faz parte da sequência de Fibonacci.')
-} else {
-  alert('O número digitado não faz parte da sequência de Fibonacci.')
-}
-
-*/
-
+ 
 let numInput = document.getElementById('number')
 let form = document.getElementById('form')
 let resultado = document.getElementById('result')
